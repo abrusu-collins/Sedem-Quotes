6c6fbfb90b97a84e0ea330b235430acb3aba2921
abrusu-collins <snillochemingson@gmail.com>
2022-11-11 14:24:31 +0000

    architecture

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+import Home from "./Home";
 
 function App() {
   return (
-    <></>
+    <>
+    <Home/>
+    </>
   );
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return ( 
+        <div className="home">
+            kjkb
+        </div>
+     );
+}
+
+export default Home;
