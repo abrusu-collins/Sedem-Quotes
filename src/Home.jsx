97d5a17f14cc8e7d778c14ab36b770e24a421cc6
abrusu-collins <snillochemingson@gmail.com>
2022-11-11 15:41:37 +0000

    nice

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,29 @@
 function Home() {
+  let thoughtArray = [
+    "At this point , the only thing that is pushing me to work hard is hunger",
+    "At this point , the only thing that is pushing me to work hard is hunger",
+    "At this point , the only thing that is pushing me to work hard is hunger",
+    "At this point , the only thing that is pushing me to work hard is hunger",
+    "At this point , the only thing that is pushing me to work hard is hunger",
+    "At this point , the only thing that is pushing me to work hard is hunger",
+    "At this point , the only thing that is pushing me to work hard is hunger",
+    "At this point , the only thing that is pushing me to work hard is hunger",
+    "At this point , the only thing that is pushing me to work hard is hunger",
+  ];
+  let i = 1;
+  let thoughtdivs = thoughtArray.map((eachThought) => {
+    return (
+      <div key={i++}>
+        <p>{eachThought} </p>
+      </div>
+    );
+  });
   return (
     <div className="home">
       <div className="hero">
         <p>Welcome to my thought board💭</p>
       </div>
-      <div className="main">
-        
-      </div>
+      <div className="main">{thoughtdivs}</div>
     </div>
   );
 }
