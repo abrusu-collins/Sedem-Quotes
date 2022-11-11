f2ce5f86bfe353f18650dff7ee97ebd9801766a0
abrusu-collins <snillochemingson@gmail.com>
2022-11-11 15:10:26 +0000

    neat

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,14 @@
 function Home() {
-    return ( 
-        <div className="home">
-            kjkb
-        </div>
-     );
+  return (
+    <div className="home">
+      <div className="hero">
+        <p>Welcome to my thought board💭</p>
+      </div>
+      <div className="main">
+        
+      </div>
+    </div>
+  );
 }
 
 export default Home;
